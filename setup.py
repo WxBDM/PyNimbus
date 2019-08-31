c7396af62040fe8f10cb538a5c9621d5cff901af
Brandon Molyneaux <bdm0041@tigermail.auburn..edu>
2019-08-31 14:31:44 -0500

    modified setup to include key words, errors with main

--- setup.py
+++ setup.py
@@ -11,22 +11,21 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-      
-      name='PyNimbus',
-      version='0.0.1',
-      long_description=README,
-      long_description_content_type="text/markdown",
-      description='A Python package to organize National Weather Service product data.',
-      url='https://github.com/WxBDM/PyNimbus',
-      author='Brandon Molyneaux',
-      author_email='bdmolyne@gmail.com',
-      license='BSD',
-      packages=['pynimbus'],
-      install_requires = ['pandas>=0.23.4'],
-      zip_safe=True,
-      classifiers=[
-        "License :: OSI Approved :: BSD",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        ],
-      )
+    name='PyNimbus',
+    version='0.1.0',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description='A Python package to organize National Weather Service product data.',
+    url='https://github.com/WxBDM/PyNimbus',
+    author='Brandon Molyneaux',
+    author_email='bdmolyne@gmail.com',
+    license='BSD 3-clause "New" or "Revised License"',
+    packages=['pynimbus'],
+    install_requires = ['pandas>=0.23.4'],
+    zip_safe=True,
+    classifiers=[
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7"],
+    keywords = "meteorology weather"
+)
