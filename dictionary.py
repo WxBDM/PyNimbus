b4242cdd72532f562926ff66c792f638fafea353
Rishab Menon <rishabmenon72@gmail.com>
2019-10-01 02:42:39 +0530

    Added Dictionary to store hurricane names

---
+++ dictionary.py
@@ -0,0 +1,81 @@
+#Dictionary to store hurricane names
+
+#Year 2019
+
+h_2019 = {  "Andrea" : ["al01", "Atlantic"],
+            "Barry" :  ["al02", "Atlantic"]
+            "Three" : ["al03", "Atlantic"]
+            "Chantal" : ["al04", "Atlantic"]
+            "Dorian" : ["al05", "Atlantic"]
+            "Erin" : ["al06", "Atlantic"]
+            "Fernand" : ["al07", "Atlantic"]
+            "Gabrielle" : ["al08", "Atlantic"]
+            "Humberto" : ["al09", "Atlantic"]
+            "Jerry" : ["al10", "Atlantic"]
+            "Imelda" : ["al11", "Atlantic"]
+            "Karen" : ["al12", "Atlantic"]
+            "Lorenzo" : ["al13", "Atlantic"]
+            "Alvin" : ["ep01", "East Pacific"],
+            "Barbara" : ["ep02", "East Pacific"],
+            "Cosme" : ["ep03", "East Pacific"],
+            "Four-e" : ["ep04", "East Pacific"],
+            "Dalila" : ["ep05", "East Pacific"],
+            "Erick" : ["ep06", "East Pacific"],
+            "Flossie" : ["ep07", "East Pacific"],
+            "Gil" : ["ep08", "East Pacific"],
+            "Henriette" : ["ep09", "East Pacific"],
+            "Ivo" : ["ep10", "East Pacific"],
+            "Juliette" : ["ep11", "East Pacific"],
+            "Akoni" : ["ep12", "East Pacific"],
+            "Kiko" : ["ep13", "East Pacific"],
+            "Mario" : ["ep14", "East Pacific"],
+            "Lorena" : ["ep15", "East Pacific"],
+            "Narda" : ["ep16", "East Pacific"],
+        }
+#-------------------------------------------------------
+
+#Year 2018
+
+
+h_2018 = {  "Alberto" : ["al01", "Atlantic"],
+            "Beryl" :  ["al02", "Atlantic"]
+            "Chris" : ["al03", "Atlantic"]
+            "Debby" : ["al04", "Atlantic"]
+            "Ernesto" : ["al05", "Atlantic"]
+            "Florence" : ["al06", "Atlantic"]
+            "Gordon" : ["al07", "Atlantic"]
+            "Helene" : ["al08", "Atlantic"]
+            "Isaac" : ["al09", "Atlantic"]
+            "Joyce" : ["al10", "Atlantic"]
+            "Eleven" : ["al11", "Atlantic"]
+            "Kirk" : ["al12", "Atlantic"]
+            "Leslie" : ["al13", "Atlantic"]
+            "Michael" : ["al14", "Atlantic"]
+            "Nadine" : ["al15", "Atlantic"]
+            "Oscar" : ["al16", "Atlantic"]
+            "Walaka" : ["ep01", "East Pacific"],
+            "Aletta" : ["ep02", "East Pacific"],
+            "Bud" : ["ep03", "East Pacific"],
+            "Carlotta" : ["ep04", "East Pacific"],
+            "Daniel" : ["ep05", "East Pacific"],
+            "Emilia" : ["ep06", "East Pacific"],
+            "Fabio" : ["ep07", "East Pacific"],
+            "Gilma" : ["ep08", "East Pacific"],
+            "Nine-e" : ["ep09", "East Pacific"],
+            "Hector" : ["ep10", "East Pacific"],
+            "Ileana" : ["ep11", "East Pacific"],
+            "John" : ["ep12", "East Pacific"],
+            "Kristy" : ["ep13", "East Pacific"],
+            "Lane" : ["ep14", "East Pacific"],
+            "Miriam" : ["ep15", "East Pacific"],
+            "Norman" : ["ep16", "East Pacific"],
+            "Olivia" : ["ep17", "East Pacific"],
+            "Paul" : ["ep18", "East Pacific"],
+            "Nineteen-e" : ["ep19", "East Pacific"],
+            "Rosa" : ["ep20", "East Pacific"],
+            "Sergio" : ["ep21", "East Pacific"],
+            "Tara" : ["ep22", "East Pacific"],
+            "Vicente" : ["ep23", "East Pacific"],
+            "Willa" : ["ep24", "East Pacific"],
+            "Xavier" : ["ep25", "East Pacific"],
+        }
